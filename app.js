ebbb0dc6f1926ffaa1e4936b9335c75a11e66476
Nileesha Fernando <nileeshasajani@hotmail.com>
2016-06-20 12:41:21 -0400

    Inserts message in database

---
+++ app.js
@@ -0,0 +1,88 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var promise = require('bluebird');
+
+var routes = require('./routes/index');
+var users = require('./routes/users');
+
+var app = express();
+var options =  {
+  promiseLib: promise
+};
+var pgp = require('pg-promise')(options);
+
+var db = pgp('postgres://localhost:5432/nileesha');
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', routes);
+app.use('/users', users);
+
+app.post('/',function(req,res,next){
+  var newMessage = req.body;
+  // expects no rows
+  db.none('INSERT INTO toDo(description)'+
+    'values(${description})',
+    req.body)
+  .then(function(){
+    res.redirect('/');
+  })
+  .catch(function (err){
+    return next(err);
+  });
+
+  
+});
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
+
+
+module.exports = app;
+
+app.listen(3000);
+
+module.exports = app;
